95d66750d30679c957a92a8a323eae321141d0ad
Dinar <123669430+Dinfat696@users.noreply.github.com>
2024-02-20 22:44:49 +0300

    first commit

---
+++ src/test/java/com/example/Test_Contacts/TestContactsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Test_Contacts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestContactsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
