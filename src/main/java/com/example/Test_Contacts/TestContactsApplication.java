678b0d43d1ff8678ceb2e4c180a2e6d4a7040645
Dinar <123669430+Dinfat696@users.noreply.github.com>
2024-02-20 22:43:32 +0300

    first commit

---
+++ src/main/java/com/example/Test_Contacts/TestContactsApplication.java
@@ -0,0 +1,13 @@
+package com.example.Test_Contacts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestContactsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestContactsApplication.class, args);
+	}
+
+}
